8676a6045b4bad949dce911017fbf9af9d69ddd0
Aaron Roberts <aaron.roberts@chime.com>
2021-12-08 20:17:59 -0500

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,82 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "flipper-follower" is now active!');
+
+  let timeout: NodeJS.Timer | undefined = undefined;
+
+  const baseUrl = vscode.workspace.getConfiguration("flipper-follower").get("baseUrl");
+  const flipperDecorationType = vscode.window.createTextEditorDecorationType({
+    textDecoration: "underline",
+  });
+
+  let activeEditor = vscode.window.activeTextEditor;
+
+  function updateDecorations() {
+    if (!activeEditor) {
+      return;
+    }
+
+    const regex = /Flipper\[["'](?<key>.+)["']\]/g;
+    const text = activeEditor.document.getText();
+    const matches: vscode.DecorationOptions[] = [];
+    let match;
+    while ((match = regex.exec(text))) {
+      const startPos = activeEditor.document.positionAt(match.index);
+      const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+      const decoration: vscode.DecorationOptions = {
+        range: new vscode.Range(startPos, endPos),
+        hoverMessage: `${baseUrl}${match.groups?.key}`,
+      };
+      matches.push(decoration);
+    }
+    console.log(matches);
+    activeEditor.setDecorations(flipperDecorationType, matches);
+  }
+
+  function triggerUpdateDecorations(throttle: boolean = false) {
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = undefined;
+    }
+    if (throttle) {
+      timeout = setTimeout(updateDecorations, 500);
+    } else {
+      updateDecorations();
+    }
+  }
+
+  if (activeEditor) {
+    triggerUpdateDecorations();
+  }
+
+  vscode.window.onDidChangeActiveTextEditor(
+    (editor) => {
+      activeEditor = editor;
+      if (editor) {
+        triggerUpdateDecorations();
+      }
+    },
+    null,
+    context.subscriptions
+  );
+
+  vscode.workspace.onDidChangeTextDocument(
+    (event) => {
+      if (activeEditor && event.document === activeEditor.document) {
+        triggerUpdateDecorations(true);
+      }
+    },
+    null,
+    context.subscriptions
+  );
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
